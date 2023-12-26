e48ade3a31beb9c591c380f8b0fcc1aaf73a4cbe
cheffuller <cheffuller@gmail.com>
2023-12-26 13:38:43 -0600

    edit to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import { Container } from 'react-bootstrap';
 
 export default function Header() {
   return (
-    <header>
+    <header className='bg-dark py-5'>
       <Container className='px-4 px-lg-5 my-5'>
-        <div class='text-center text-white'>
-          <h1 class='display-4 fw-bolder'>The Walk-In - Testing</h1>
-          <p class='lead fw-normal text-white-50 mb-0'>
+        <div className='text-center text-white'>
+          <h1 className='display-4 fw-bolder'>The Walk-In - Testing</h1>
+          <p className='lead fw-normal text-white-50 mb-0'>
             do all of your restaurant's orders in one place
           </p>
-          <p class='lead fw-normal text-white-50 mb-0'>
+          <p className='lead fw-normal text-white-50 mb-0'>
             we are currently under construction and serving up dummy data
           </p>
         </div>
