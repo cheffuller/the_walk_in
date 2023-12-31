051868cfefa42599805ea4d36d6a3e3ec0eece5b
cheffuller <cheffuller@gmail.com>
2023-12-30 21:24:41 -0600

    edits to database/sequelize info

--- src/server.js
+++ src/server.js
@@ -23,8 +23,6 @@ require("./routes/product.routes.js")(app);
 require("./routes/cart__product.routes.js")(app);
 require("./routes/company__vendor.routes.js")(app);
 
-const PORT = process.env.PORT || 8080; // Port
-
 sequelize
   .authenticate()
   .then(() => {
@@ -35,7 +33,5 @@ sequelize
   });
 
 sequelize.sync({alter:true}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });
+    console.log(`Database creation/alteration successful`);
 });
