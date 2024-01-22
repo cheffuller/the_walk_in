9e680bfd5b5031ddac284541873205ef9685fa38
cheffuller <cheffuller@gmail.com>
2024-01-22 17:56:32 -0600

    login functionality

--- src/frontend/components/User/UserNew.js
+++ src/frontend/components/User/UserNew.js
@@ -7,7 +7,7 @@ import { handleEditChange } from '../../lib/handleEditChange';
 import EditMessage from '../../lib/EditMessage';
 
 const UserNew = () => {
-  const { user } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   const [appUser, setAppUser] = useState({ username: '' });
   const [companies, setCompanies] = useState([]);
   const [message, setMessage] = useState();
@@ -16,6 +16,7 @@ const UserNew = () => {
     (async () => {
       const res = await axios.get(`${process.env.REACT_APP_API_URL}company`);
       setCompanies(res.data);
+      console.log(user)
       if (user.nickname) {
         setAppUser({ ...appUser, username: user.nickname });
       }
